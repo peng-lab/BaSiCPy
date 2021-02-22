114d0070f087dc8708e6e9adf46731ccd86ec683
Mirkazemi <mohammad.mirkazemi@gmail.com>
2021-02-22 10:38:05 +0100

    removing the examples

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
-from setuptools import setup, find_packagesfrom pathlib import Pathsetup(      name = 'pybasic',      install_requires=[          l.strip() for l in Path('requirements.txt').read_text('utf-8').splitlines()      ])
+from setuptools import setup, find_packages
+from pathlib import Path
+
+setup(
+      name = 'pybasic',
+      install_requires=[
+          l.strip() for l in Path('requirements.txt').read_text('utf-8').splitlines()
+      ]
+)
