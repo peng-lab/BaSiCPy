8422501852b4ba25ee4f6ce588b2b452718ab989
Mirkazemi <mohammad.mirkazemi@gmail.com>
2021-04-08 16:28:11 +0200

    "editting init"

--- pybasic/__init__.py
+++ pybasic/__init__.py
@@ -7,7 +7,7 @@ Created on Thu Dec  5 17:32:25 2019
 """
 
 from . import tools
-from ._background import background_timelapse, basic
+from ._background import background_timelapse, basic, correct_illumination
 from ._settings import PyBasicConfig
 settings = PyBasicConfig()
 
