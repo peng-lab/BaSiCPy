92888e469b82e52913ad84e7836373fa412cf424
Mirkazemi <mohammad.mirkazemi@gmail.com>
2021-09-15 11:38:44 +0200

    Add authors, emails and keywords to the setup

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 from pathlib import Path
 
 setup(
-      name = 'pybasic',
-      install_requires=[
-          l.strip() for l in Path('requirements.txt').read_text('utf-8').splitlines()
-      ]
+    name = 'pybasic',
+    author='Lorenz Lamm, Tingying Peng, Mohammad Mirkazemi',
+    author_email='tingying.peng@helmholtz-muenchen.de, Lorenz.lamm@helmholtz-muenchen.de',
+    keywords='Background Shading Flatfield Darkfield Biology Optical Microscopy Image',
+    install_requires=[
+        l.strip() for l in Path('requirements.txt').read_text('utf-8').splitlines()
+    ]
 )
