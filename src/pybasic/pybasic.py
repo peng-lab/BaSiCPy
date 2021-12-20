5c8411e4e99d5a0c2ccc92a215a4b800fde4c4c7
Tim Morello <tdmorello@gmail.com>
2021-12-20 18:10:49 -0500

    Placeholder dct functions

--- src/pybasic/pybasic.py
+++ src/pybasic/pybasic.py
@@ -4,6 +4,8 @@ import numpy as np
 
 
 class BaSiC:
+    _use_gpu: bool
+
     def __init__(self):
         ...
 
@@ -13,6 +15,30 @@ class BaSiC:
 
     def predict(self, image: np.ndarray):
         """Apply model"""
+        ...
+
+    def _dct(self):
+        if self._use_gpu:
+            return self._dct_gpu()
+        else:
+            return self._dct_cpu()
+    
+    def _idct(self):
+        if self._use_gpu:
+            return self._idct_gpu()
+        else:
+            return self._idct_cpu()
+
+    def _dct_gpu(self):
+        ...
+    
+    def _dct_cpu(self):
+        ...
+
+    def _idct_gpu(self):
+        ...
+
+    def _idct_cpu(self):
         ...
 
     def __repr__(self):
