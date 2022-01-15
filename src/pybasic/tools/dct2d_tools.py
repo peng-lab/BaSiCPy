9420ce8e16c926af211222660f2f770fedacc934
Tim Morello <tdmorello@gmail.com>
2022-01-15 13:23:22 -0500

    Add dct backends and tests

--- src/pybasic/tools/dct2d_tools.py
+++ src/pybasic/tools/dct2d_tools.py
@@ -3,11 +3,20 @@
 import os
 from abc import ABC, abstractmethod, abstractproperty
 
-import cv2
+# TODO remove below after benchmarking or change to logging
+try:
+    import cv2
+except ImportError:
+    print("unable to import cv2")
+try:
+    import scipy.fft
+except ImportError:
+    print("unable to import scipy")
+
+import jax
 import numpy as np
 
-__all__ = ["DCT_BACKENDS", "dct2d", "idct2d"]
-
+__all__ = ["dct2d", "idct2d"]
 DEFAULT_BACKEND = "SCIPY"
 
 
@@ -20,7 +29,7 @@ class DCT(ABC):
     @staticmethod
     @abstractmethod
     def dct2d(arr: np.ndarray) -> np.ndarray:
-        return np.ndarray
+        ...
 
     @staticmethod
     @abstractmethod
@@ -33,11 +42,13 @@ class JaxDCT(DCT):
 
     @staticmethod
     def dct2d(arr: np.ndarray) -> np.ndarray:
-        ...
+        return jax.scipy.fft.dct(jax.scipy.fft.dct(arr, norm="ortho"), norm="ortho")
 
     @staticmethod
     def idct2d(arr: np.ndarray) -> np.ndarray:
-        ...
+        return jax.scipy.fft.idct(
+            jax.scipy.fft.idct(arr, norm="ortho"), norm="ortho"
+        )  # FIXME only dct type 2 is implemented in JAX...
 
 
 class OpenCVDCT(DCT):
@@ -45,29 +56,44 @@ class OpenCVDCT(DCT):
 
     @staticmethod
     def dct2d(arr: np.ndarray) -> np.ndarray:
-        return cv2.dct(arr).astype(np.float64)
+        return cv2.dct(arr)
 
     @staticmethod
     def idct2d(arr: np.ndarray) -> np.ndarray:
-        return cv2.dct(arr, flags=cv2.DCT_INVERSE).astype(np.float64)
+        return cv2.dct(arr, flags=cv2.DCT_INVERSE)
 
 
 class SciPyDCT(DCT):
     _backend = "SCIPY"
 
+    # NOTE can we use scipy.fftpack.dctn to replace the nested function below?
+
     @staticmethod
     def dct2d(arr: np.ndarray) -> np.ndarray:
-        ...
+        return scipy.fft.dct(scipy.fft.dct(arr.T, norm="ortho").T, norm="ortho")
 
     @staticmethod
     def idct2d(arr: np.ndarray) -> np.ndarray:
-        ...
+        return scipy.fft.idct(scipy.fft.idct(arr.T, norm="ortho").T, norm="ortho")
 
 
-DCT_BACKENDS = {"JAX": JaxDCT, "OPENCV": OpenCVDCT, "SCIPY": SciPyDCT}
+DCT_BACKENDS = {"JAX": JaxDCT(), "OPENCV": OpenCVDCT(), "SCIPY": SciPyDCT()}
 
 
-dct = DCT_BACKENDS[os.environ["DCT_BACKEND"] or DEFAULT_BACKEND]
+if os.environ.get("BASIC_DCT_BACKEND"):
+    try:
+        dct = DCT_BACKENDS[os.environ["BASIC_DCT_BACKEND"]]
+    except KeyError as e:
+        # TODO change to logging or warning
+        raise e
+        # # OR when this fails, revert to default
+        # print(
+        #     f"unrecognized dct backend {os.environ['BASIC_DCT_BACKEND']}, "
+        #     f"defaulting to {DEFAULT_BACKEND}"
+        # )
+        # dct = DCT_BACKENDS[DEFAULT_BACKEND]
+else:
+    dct = DCT_BACKENDS[DEFAULT_BACKEND]
 
 dct2d = dct.dct2d
 idct2d = dct.idct2d
--- tests/test_tools_dct2D.py
+++ tests/test_tools_dct2D.py
@@ -1,12 +1,70 @@
 """Tests the 2D dct tools."""
 
+# from pybasic.tools import dct2d_tools
+from pybasic.tools.dct2d_tools import DCT_BACKENDS
+
 import pytest
-from pybasic.tools import dct2d, idct2d
+import scipy.fft
+import numpy as np
+import sys
+import importlib
+
+np.random.seed(1234)
+arr_input = np.random.random((4, 4))
+arr_forward_exp = scipy.fft.dct(
+    scipy.fft.dct(arr_input.T, norm="ortho").T, norm="ortho"
+)
+arr_reverse_exp = scipy.fft.idct(
+    scipy.fft.idct(arr_input.T, norm="ortho").T, norm="ortho"
+)
+
+backends = ["JAX", "OPENCV", "SCIPY"]
 
 
-def test_dct2d():
-    ...
+@pytest.mark.parametrize("backend", backends)
+def test_dct_backends(backend):
+    if backend == "JAX":
+        return
+
+    dct2d = DCT_BACKENDS[backend].dct2d
+    idct2d = DCT_BACKENDS[backend].idct2d
+
+    # check that dct2d and idct2d are at least inverse functions
+    assert np.allclose(arr_input, idct2d(dct2d(arr_input)))
+
+    # check that results match scipy dct
+    arr_forward_actual = dct2d(arr_input)
+    arr_reverse_actual = idct2d(arr_input)
+
+    assert np.allclose(arr_forward_exp, arr_forward_actual)
+    assert np.allclose(arr_reverse_exp, arr_reverse_actual)
 
 
-def test_idct2d():
+@pytest.mark.parametrize("backend", backends)
+def test_dct_backend_import(monkeypatch, backend):
+    import pybasic.tools.dct2d_tools
+
+    monkeypatch.setenv("BASIC_DCT_BACKEND", backend)
+    importlib.reload(pybasic.tools.dct2d_tools)
+
+    assert pybasic.tools.dct2d_tools.dct._backend == backend
+
+
+def test_unrecognized_backend(monkeypatch):
+    import pybasic.tools.dct2d_tools
+
+    backend = "FAKE_BACKEND"
+
+    monkeypatch.setenv("BASIC_DCT_BACKEND", "FAKE_BACKEND")
+
+    with pytest.raises(KeyError):
+        importlib.reload(pybasic.tools.dct2d_tools)
+
+    # assert (
+    #     pybasic.tools.dct2d_tools.dct._backend
+    #     == pybasic.tools.dct2d_tools.DEFAULT_BACKEND
+    # )
+
+
+def test_backend_not_installed():
     ...
