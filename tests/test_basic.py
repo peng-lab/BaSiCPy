6a447932ee9c1b790f52ee1c439c6561555922ab
Nicholas-Schaub <nicholas.j.schaub@gmail.com>
2022-01-17 18:09:09 -0500

    Implemented shading model fit algorithm from main branch (#27)
    * Implemented shading model fit algorithm from main branch
    
    * Fixed idct in inexact_alm.py

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,23 +1,10 @@
 from pybasic import BaSiC
 import numpy as np
+import pytest
 
 
-# DEBUG fails because Settings is now a pydantic dataclass
-def test_basic_verify_init():
-
-    basic = BaSiC()
-
-    assert all([d == 128 for d in basic.darkfield.shape])
-    assert all([d == 128 for d in basic.flatfield.shape])
-
-    return
-
-
-def test_basic():
-    ...
-
-
-def test_basic_fit(capsys):
+@pytest.fixture
+def test_data():
 
     np.random.seed(42)  # answer to the meaning of life, should work here too
 
@@ -40,6 +27,45 @@ def test_basic_fit(capsys):
     images = np.random.poisson(lam=gradient_int.flatten(), size=(n_images, size ** 2))
     images = images.transpose().reshape((size, size, n_images))
 
+    return gradient, images, truth
+
+
+# Ensure BaSiC initialization passes pydantic type checking
+def test_basic_verify_init():
+
+    basic = BaSiC()
+
+    assert all([d == 128 for d in basic.darkfield.shape])
+    assert all([d == 128 for d in basic.flatfield.shape])
+
+    return
+
+
+# Test BaSiC fitting function
+def test_basic_fit(capsys, test_data):
+
+    basic = BaSiC(get_darkfield=False)
+
+    gradient, images, truth = test_data
+
+    """Fit with BaSiC"""
+    basic.fit(images)
+
+    # TODO: Implement correctness checks
+
+    # for human error checking
+    # with capsys.disabled():
+    #     print()
+    #     print(truth[60:70, 60:70])
+    #     print(basic.flatfield[60:70, 60:70])
+
+
+# Test BaSiC predict function
+def test_basic_predict(capsys, test_data):
+
+    basic = BaSiC(get_darkfield=False)
+    gradient, images, truth = test_data
+
     """Apply the shading model to the images"""
     # flatfield only
     basic.flatfield = gradient
@@ -53,5 +79,5 @@ def test_basic_fit(capsys):
     assert corrected.mean() < corrected_error
 
     """Test shortcut"""
-    correct = basic(images)
+    corrected = basic(images)
     assert corrected.mean() < corrected_error
--- tests/test_tools_inexact_alm.py
+++ tests/test_tools_inexact_alm.py
@@ -1,8 +1,5 @@
 """Test the Inexact Augmented Lagrangian Method function."""
 
-import pytest
-from pybasic.tools.inexact_alm import inexact_alm_l1
-
 
 def test_inexact_alm_l1():
     ...
