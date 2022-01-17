79914a2e8180f4801076407efb647f7afdb8100b
Nicholas-Schaub <nicholas.j.schaub@gmail.com>
2022-01-17 17:17:35 -0500

    Added predict method, does not include baseline estimation (#29)

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,5 @@
 from pybasic import BaSiC
+import numpy as np
 
 
 # DEBUG fails because Settings is now a pydantic dataclass
@@ -14,3 +15,43 @@ def test_basic_verify_init():
 
 def test_basic():
     ...
+
+
+def test_basic_fit(capsys):
+
+    np.random.seed(42)  # answer to the meaning of life, should work here too
+
+    n_images = 8
+    basic = BaSiC(get_darkfield=False)
+
+    """Generate a parabolic gradient to simulate uneven illumination"""
+    # Create a gradient
+    size = basic.working_size
+    grid = np.meshgrid(*(2 * (np.linspace(-size // 2 + 1, size // 2, size),)))
+
+    # Create the gradient (flatfield) with and offset (darkfield)
+    gradient = sum(d ** 2 for d in grid) ** (1 / 2) + 8
+    gradient_int = gradient.astype(np.uint8)
+
+    # Ground truth, for correctness checking
+    truth = gradient / gradient.mean()
+
+    # Create an image stack and add poisson noise
+    images = np.random.poisson(lam=gradient_int.flatten(), size=(n_images, size ** 2))
+    images = images.transpose().reshape((size, size, n_images))
+
+    """Apply the shading model to the images"""
+    # flatfield only
+    basic.flatfield = gradient
+    corrected = basic.predict(images)
+    corrected_error = corrected.mean()
+    assert corrected_error < 0.5
+
+    # with darkfield correction
+    basic.darkfield = np.full(basic.flatfield.shape, 8)
+    corrected = basic.predict(images)
+    assert corrected.mean() < corrected_error
+
+    """Test shortcut"""
+    correct = basic(images)
+    assert corrected.mean() < corrected_error
