43a819e4f5f202edc220f63f02f70e8befdb9468
Tim Morello <tdmorello@gmail.com>
2022-01-20 00:06:37 -0500

    Remove Profiles class and methods from basic

--- src/pybasic/__init__.py
+++ src/pybasic/__init__.py
@@ -1,6 +1,5 @@
 """Compute an illumination correction model for mosaic microscope images."""
 
-from pybasic.profile import Profile
 from pybasic.pybasic import BaSiC
 
-__all__ = ["BaSiC", "Profile"]
+__all__ = ["BaSiC"]
