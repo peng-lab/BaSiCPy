f5473567f4702187c8e8feb7756cb76e56534dd8
Yohsuke Fukai <ysk@yfukai.net>
2022-04-11 20:13:51 +0900

    added files

--- src/basicpy/__init__.py
+++ src/basicpy/__init__.py
@@ -1,5 +1,6 @@
 """Compute an illumination correction model for mosaic microscope images."""
 
 from basicpy.basicpy import BaSiC
+from basicpy import data
 
-__all__ = ["BaSiC"]
+__all__ = ["BaSiC", "data"]
