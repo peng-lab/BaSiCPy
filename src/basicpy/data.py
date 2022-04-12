8073e01c1d5ce312b7dc9ff07f272b77f6e5ca94
Yohsuke Fukai <ysk@yfukai.net>
2022-04-12 20:22:23 +0900

    added fetch code to data module

---
+++ src/basicpy/data.py
@@ -0,0 +1,67 @@
+import pooch
+from os import path
+import glob
+from skimage.io import imread
+
+EXPERIMENTAL_TEST_DATA_NAMES = {
+    "Cell_culture.zip": "md5:797bbc4c891e5fe59f4200e771b55c3a",
+    "Timelapse_brightfield.zip": "md5:460e5f78ac69856705704fedad9f9e59",
+    "Timelapse_nanog.zip.zip": "md5:815d53cac35b671269b17bd627d7baa7",
+    "Timelapse_Pu1.zip.zip": "md5:bee97561e87c51e90b46da9b439e8b7b",
+    "WSI_Brain.zip": "md5:6e163786ddec2a690aa4bb47a64bcded",
+}
+
+POOCH = pooch.create(
+    path=pooch.os_cache("testdata"),
+    # Use the Zenodo DOI
+    base_url="doi:10.5281/zenodo.6334810/",
+    registry=EXPERIMENTAL_TEST_DATA_NAMES,
+)
+
+
+def fetch(data_name: str):
+    test_file_paths = POOCH.fetch(data_name, processor=pooch.Unzip())
+    assert all(path.exists(f) for f in test_file_paths)
+    basedir = path.commonpath(test_file_paths)
+    uncorrected_paths = sorted(
+        glob.glob(path.join(basedir, "Uncorrected*", "**", "*.tif"), recursive=True)
+    )
+    if len(uncorrected_paths) == 0:
+        uncorrected_paths = sorted(
+            glob.glob(path.join(basedir, "Uncorrected*", "**", "*.png"), recursive=True)
+        )
+    corrected_paths = sorted(
+        glob.glob(path.join(basedir, "Corrected*", "**", "*.tif"), recursive=True)
+    )
+    if "WSI_Brain" in data_name:
+        uncorrected_paths = list(
+            filter(lambda p: "BrainSection" in p, uncorrected_paths)
+        )
+        corrected_paths = list(filter(lambda p: "BrainSection" in p, corrected_paths))
+
+    assert len(uncorrected_paths) > 0
+    assert len(uncorrected_paths) == len(corrected_paths)
+    uncorrected = (imread(f) for f in uncorrected_paths)
+    corrected = (imread(f) for f in corrected_paths)
+
+    return uncorrected, corrected
+
+
+def cell_culture():
+    return fetch("Cell_culture.zip")[0]
+
+
+def timelapse_brightfield():
+    return fetch("Timelapse_brightfield.zip")[0]
+
+
+def timelapse_nanog():
+    return fetch("Timelapse_nanog.zip.zip")[0]
+
+
+def timelapse_pu1():
+    return fetch("Timelapse_Pu1.zip.zip")[0]
+
+
+def wsi_brain():
+    return fetch("WSI_Brain.zip")[0]
