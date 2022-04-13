97afaf06bc9d1a0aca50a1d756b88b1bf3d6b305
Yohsuke Fukai <ysk@yfukai.net>
2022-04-13 12:07:21 +0900

    fixed typo...

--- tests/test_data.py
+++ tests/test_data.py
@@ -7,8 +7,8 @@ def test_fetch():
     """Test fetching the data for all datasets."""
     for k in data.EXPERIMENTAL_TEST_DATA_PROPS.keys():
         uncorrected, corrected = data.fetch(k)
-        uncorrected = np.array(uncorrected)
-        corrected = np.array(corrected)
+        uncorrected = np.array(list(uncorrected))
+        corrected = np.array(list(corrected))
 
         assert uncorrected.ndim == 3
         assert corrected.ndim == 3
