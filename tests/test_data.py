78ae1e44b12c64dd2c2230bbf6e23f807a594dfd
Yohsuke Fukai <ysk@yfukai.net>
2022-04-13 12:35:09 +0900

    parametrized tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,15 +1,16 @@
 from re import I
 from basicpy import data
 import numpy as np
+import pytest
 
 
-def test_fetch():
+@pytest.mark.parametrize("data_name", data.EXPERIMENTAL_TEST_DATA_PROPS.keys())
+def test_fetch(data_name):
     """Test fetching the data for all datasets."""
-    for k in data.EXPERIMENTAL_TEST_DATA_PROPS.keys():
-        uncorrected, corrected = data.fetch(k)
-        uncorrected = np.array(list(uncorrected))
-        corrected = np.array(list(corrected))
+    uncorrected, corrected = data.fetch(data_name)
+    uncorrected = np.array(list(uncorrected))
+    corrected = np.array(list(corrected))
 
-        assert uncorrected.ndim == 3
-        assert corrected.ndim == 3
-        assert np.array_equal(uncorrected.shape, corrected.shape)
+    assert uncorrected.ndim == 3
+    assert corrected.ndim == 3
+    assert np.array_equal(uncorrected.shape, corrected.shape)
