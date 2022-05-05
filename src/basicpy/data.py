4b30d24e0be67efb4564b9af9f8e9497e39e2773
Yohsuke Fukai <ysk@yfukai.net>
2022-05-05 11:27:25 +0900

    Revert "disabled hash checking"
    This reverts commit d0dbd8ab283b9db861775905d7b18c55db01cf38.

--- src/basicpy/data.py
+++ src/basicpy/data.py
@@ -6,18 +6,23 @@ from skimage.io import imread
 EXPERIMENTAL_TEST_DATA_PROPS = {
     "cell_culture": {
         "filename": "Cell_culture.zip",
+        "hash": "md5:797bbc4c891e5fe59f4200e771b55c3a",
     },
     "timelapse_brightfield": {
         "filename": "Timelapse_brightfield.zip",
+        "hash": "md5:460e5f78ac69856705704fedad9f9e59",
     },
     "timelapse_nanog": {
         "filename": "Timelapse_nanog.zip.zip",
+        "hash": "md5:815d53cac35b671269b17bd627d7baa7",
     },
     "timelapse_pu1": {
         "filename": "Timelapse_Pu1.zip.zip",
+        "hash": "md5:bee97561e87c51e90b46da9b439e8b7b",
     },
     "wsi_brain": {
         "filename": "WSI_Brain.zip",
+        "hash": "md5:6e163786ddec2a690aa4bb47a64bcded",
     },
 }
 
@@ -46,7 +51,7 @@ def fetch(data_name: str):
     """
     if data_name not in EXPERIMENTAL_TEST_DATA_PROPS.keys():
         raise ValueError(f"{data_name} is not a valid test data name")
-    file_name: str = EXPERIMENTAL_TEST_DATA_PROPS[data_name]["filename"]
+    file_name = EXPERIMENTAL_TEST_DATA_PROPS[data_name]["filename"]
     test_file_paths = POOCH.fetch(file_name, processor=pooch.Unzip())
     assert all(path.exists(f) for f in test_file_paths)
     basedir = path.commonpath(test_file_paths)
