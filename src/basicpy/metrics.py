d9d7478a81a6c65161e088dacf6fa4002d87e77e
Yohsuke Fukai <ysk@yfukai.net>
2023-04-20 18:59:37 +0900

    updated entropy routine

--- src/basicpy/metrics.py
+++ src/basicpy/metrics.py
@@ -1,16 +1,18 @@
 import numpy as np
 
 
-def entropy(image_float, qmin=0.01, qmax=0.99, ignore_zeros=True):
+def entropy(image_float, qmin=0.01, qmax=0.99, ignore_zeros=True, bins=256):
     """Calculate the entropy of an image.
     Parameters
     ----------
     image_float : array
         The image.
-    vmin : float
-        The minimum value of the image.
-    vmax : float
-        The maximum value of the image.
+    qmin : float
+        The minimum quantile of the image.
+    qmax : float
+        The maximum quantile of the image.
+    bins : int
+        The number of bins to use for the histogram.
     Returns
     -------
     entropy : float
@@ -19,8 +21,7 @@ def entropy(image_float, qmin=0.01, qmax=0.99, ignore_zeros=True):
     vmin, vmax = np.quantile(image_float, [qmin, qmax])
     image = (image_float - vmin) / (vmax - vmin)
     image = np.clip(image, 0, 1)
-    image = (image * 255).astype(np.uint8)
-    hist = np.histogram(image, bins=256, range=(0, 256))[0]
+    hist = np.histogram(image, bins=bins, range=(0, 1))[0]
     if ignore_zeros:
         hist = hist[hist > 0]
     hist = hist / hist.sum()
