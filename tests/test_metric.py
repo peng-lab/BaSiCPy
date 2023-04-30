6a8f27db2fa4efcd3c56117c55d9fb2a23fbdd45
Yohsuke Fukai <ysk@yfukai.net>
2023-04-30 19:31:21 +0900

    fixed test bug

--- tests/test_metric.py
+++ tests/test_metric.py
@@ -5,8 +5,8 @@ from basicpy.metrics import entropy
 
 def test_entropy():
     rand_vals = np.random.rand(1000000) * 0.5
-    entropy = entropy(rand_vals, 0, 0.5, bins=100)
-    assert np.isclose(entropy, np.log(0.5), atol=0.01)
+    entropy_val = entropy(rand_vals, 0, 0.5, bins=100)
+    assert np.isclose(entropy_val, np.log(0.5), atol=0.01)
     rand_vals = np.random.normal(scale=1.5, size=1000000)
-    entropy = entropy(rand_vals, -10, 10, bins=1000)
-    assert np.isclose(entropy, 0.5 * (np.log(2 * np.pi * 1.5**2) + 1), atol=0.01)
+    entropy_val = entropy(rand_vals, -10, 10, bins=1000)
+    assert np.isclose(entropy_val, 0.5 * (np.log(2 * np.pi * 1.5**2) + 1), atol=0.01)
