94ef8fcb10e5dffb8234222dc511e82923c700e9
Nicholas-Schaub <nicholas.j.schaub@gmail.com>
2023-05-05 07:31:44 -0400

    Bump version: 1.1.0-dev0 → 1.1.0

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ author = "BaSiCPy collaboration"
 
 # The full version, including alpha/beta/rc tags
 
-release = "1.1.0-dev0"
+release = "1.1.0"
 
 
 # -- General configuration ---------------------------------------------------
