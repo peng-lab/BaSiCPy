646c6bb3309580f718529ce8b1ed563f8e25348b
Yohsuke Fukai <ysk@yfukai.net>
2023-06-05 15:24:33 +0900

    fixing bugs

--- src/basicpy/metrics.py
+++ src/basicpy/metrics.py
@@ -41,8 +41,6 @@ def entropy(
     )
     # density : p(x) ... normalized such that the integral over the range is 1
     dx = edges[1] - edges[0]
-    print(dx)
-    print(np.max(edges[1:] - edges[:-1] - dx))
     assert np.allclose(dx, edges[1:] - edges[:-1], atol=np.max(edges) * 0.01, rtol=0.01)
     assert np.isclose(np.sum(prob_density) * dx, 1)
     prob_density = prob_density[prob_density > 0]
